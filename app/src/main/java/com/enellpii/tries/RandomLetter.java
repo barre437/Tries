85685cb871192e6021fffe035a6bad0b65f4920d
Melissa Barrett <barre437@d.umn.edu>
2017-06-29 11:03:48 -0500

    Multiplied letter frequency array by 10 to include spaces for j, x, q, and z. Still very unlikely to be generated.

--- app/src/main/java/com/enellpii/tries/RandomLetter.java
+++ app/src/main/java/com/enellpii/tries/RandomLetter.java
@@ -8,60 +8,68 @@ import java.util.Random;
  */
 
 public class RandomLetter {
-    char[] letters = new char[200];
+    char[] letters = new char[1000];
 
     public char getLetter(){
         Random rand = new Random();
-        int i = rand.nextInt(200);
+        int i = rand.nextInt(1000);
         return this.letters[i];
     }
 
     public RandomLetter() {
-        for(int i = 0; i < 200; i++){
-            if(i < 25)
+        for(int i = 0; i < 1000; i++){
+            if(i < 127)
                 letters[i] = 'e';
-            else if(i >= 25 && i < 43)
+            else if(i >= 127 && i < 218)
                 letters[i] = 't';
-            else if(i >= 43 && i < 59)
+            else if(i >= 218 && i < 300)
                 letters[i] = 'a';
-            else if(i >= 59 && i < 74)
+            else if(i >= 300 && i < 375)
                 letters[i] = 'o';
-            else if(i >= 74 && i < 88)
+            else if(i >= 375 && i < 445)
                 letters[i] = 'i';
-            else if(i >= 88 && i < 101)
+            else if(i >= 445 && i < 512)
                 letters[i] = 'n';
-            else if(i >= 101 && i < 114)
+            else if(i >= 512 && i < 575)
                 letters[i] = 's';
-            else if(i >= 114 && i < 126)
+            else if(i >= 575 && i < 636)
                 letters[i] = 'h';
-            else if(i >= 126 && i < 138)
+            else if(i >= 636 && i < 696)
                 letters[i] = 'r';
-            else if(i >= 138 && i < 147)
+            else if(i >= 696 && i < 739)
                 letters[i] = 'd';
-            else if(i >= 147 && i < 155)
+            else if(i >= 739 && i < 779)
                 letters[i] = 'l';
-            else if(i >= 155 && i < 161)
+            else if(i >= 779 && i < 807)
                 letters[i] = 'c';
-            else if(i >= 161 && i < 167)
+            else if(i >= 807 && i < 835)
                 letters[i] = 'u';
-            else if(i >= 167 && i < 172)
+            else if(i >= 835 && i < 859)
                 letters[i] = 'm';
-            else if(i >= 172 && i < 177)
+            else if(i >= 859 && i < 883)
                 letters[i] = 'w';
-            else if(i >= 177 && i < 181)
+            else if(i >= 883 && i < 905)
                 letters[i] = 'f';
-            else if(i >= 181 && i < 185)
+            else if(i >= 905 && i < 925)
                 letters[i] = 'g';
-            else if(i >= 185 && i < 189)
+            else if(i >= 925 && i < 945)
                 letters[i] = 'y';
-            else if(i >= 189 && i < 193)
+            else if(i >= 945 && i < 964)
                 letters[i] = 'p';
-            else if(i >= 193 && i < 196)
+            else if(i >= 964 && i < 979)
                 letters[i] = 'b';
-            else if(i >= 196 && i < 198)
+            else if(i >= 979 && i < 989)
                 letters[i] = 'v';
-            else if(i >= 198 && i < 200)
+            else if(i >= 989 && i < 995)
                 letters[i] = 'k';
+            else if(i == 996)
+                letters[i] = 'j';
+            else if(i == 997)
+                letters[i] = 'x';
+            else if(i == 998)
+                letters[i] = 'q';
+            else if(i == 999)
+                letters[i] = 'z';
         }
     }
 }
